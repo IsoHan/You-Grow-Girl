918cb426200be203c24b109ec657a3052451a774
aselperera <aselperera@hotmail.com>
2021-04-30 15:16:31 +0100

    Set up initial components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+import AllPlants from './components/AllPlants';
+import Search from './components/Search';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Route path='/' exact>
+				<Homepage />
+			</Route>
+			<Route path='/allplants'>
+				<AllPlants />
+			</Route>
+			<Route path='/search'>
+				<Search />
+			</Route>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from '../images/logo.png';
+
+function Navbar() {
+	return (
+		<div>
+			<img src={logo} alt={''} />
+		</div>
+	);
+}
+
+export default Navbar;
