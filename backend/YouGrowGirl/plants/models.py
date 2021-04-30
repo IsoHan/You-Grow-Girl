efb2c9c1e27127c7e1c794f81c89d98407444889
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-04-30 23:26:43 +0100

    plants model updated and database populated

--- backend/YouGrowGirl/plants/models.py
+++ backend/YouGrowGirl/plants/models.py
@@ -5,11 +5,15 @@ from django.db import models
 # example model
 
 class Plant(models.Model):
-    common_name = models.CharField(max_length=100, unique=True, default=False)
+    common_name = models.CharField(max_length=100, unique=True)
+    img_name = models.CharField(max_length=100, default=False)
     sunlight = models.CharField(max_length=100, blank=True)
     moisture = models.CharField(max_length=500, default=False, blank=True)
-    height = models.CharField(max_length=100, default=False,  blank=True)
     toxic_to_dogs= models.BooleanField(default=False)
     toxic_to_cats= models.BooleanField(default=False)
+    plant_habit = models.CharField(max_length=500, default=False, blank=True)
+    bloom_period = models.CharField(max_length=500, default=False, blank=True)
     humidity = models.CharField(max_length=100, default=False, blank=True)
+    ph_soil = models.CharField(max_length=100, default=False, blank=True)
+    description = models.CharField(max_length=100, default=False, blank=True)
 
