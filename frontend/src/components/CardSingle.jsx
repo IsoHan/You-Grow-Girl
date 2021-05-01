225cbb81485fccf529127224aa6d5318e24ae6c6
aselperera <aselperera@hotmail.com>
2021-05-01 17:06:09 +0100

    Added some basic styling to card

--- frontend/src/components/CardSingle.jsx
+++ frontend/src/components/CardSingle.jsx
@@ -6,15 +6,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const CardSingle = () => {
 	return (
 		<div>
-			<Card style={{ width: '18rem' }}>
+			<Card
+				style={{
+					width: '18rem',
+					border: '#ffcecf solid 1px',
+					borderRadius: '30px',
+					padding: '20px 30px',
+				}}
+			>
 				<Card.Img variant='top' src={logo} />
 				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
+					<Card.Title style={{ color: '#437e85', fontSize: '35px' }}>
+						Zebra Plant
+					</Card.Title>
+					<Card.Text style={{ color: '#525252' }}>
+						A tropical plant originally from Brazil. Typically grown indoors,
+						it's lauded for its unique dark leaves that are striped with white
+						veins, as well as its colorful flowers.
 					</Card.Text>
-					<Button variant='primary'>Go somewhere</Button>
+					<Button style={{ backgroundColor: '#ffcecf', border: 'none' }}>
+						More info
+					</Button>
 				</Card.Body>
 			</Card>
 		</div>
