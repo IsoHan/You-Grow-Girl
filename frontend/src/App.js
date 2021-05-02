b696c55a972889de4b5bb0c5621d49d0a2d7946c
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-05-02 17:48:20 +0100

    added logout functionality to prototype login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,14 +16,27 @@ function App() {
 	};
 
 	const [user, setUser] = useState({ name: "", email: "" });
-	const [error, setErrror] = useState("");
+	const [error, setError] = useState("");
 
 	const Login = (details) => {
 		console.log(details);
+
+		if (
+			details.email === adminUser.email &&
+			details.password === adminUser.password
+		) {
+			console.log("Logged in");
+			setUser({
+				name: details.name,
+				email: details.email,
+			});
+		} else {
+			console.log("Details do not match!");
+		}
 	};
 
 	const Logout = () => {
-		console.log("Logout");
+		setUser({ name: "", email: "" });
 	};
 
 	return (
@@ -33,7 +46,7 @@ function App() {
 					<h2>
 						Welcome, <span>{user.name}</span>
 					</h2>
-					<button>Logout</button>
+					<button onClick={Logout}>Logout</button>
 				</div>
 			) : (
 				<Router>
