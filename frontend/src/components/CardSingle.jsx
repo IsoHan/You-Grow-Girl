e22fcd81f96abb6133f7efa024a21925d539cc96
aselperera <aselperera@hotmail.com>
2021-05-03 16:17:04 +0100

    Added second button to card

--- frontend/src/components/CardSingle.jsx
+++ frontend/src/components/CardSingle.jsx
@@ -6,6 +6,7 @@ import { useImage } from 'react-image';
 import CardColumns from 'react-bootstrap/CardColumns';
 import '../App.css';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const CardSingle = ({ title, description, image }) => {
 	function MyImageComponent() {
@@ -28,15 +29,43 @@ const CardSingle = ({ title, description, image }) => {
 			>
 				<Card.Img variant='top' src={image} />
 				<Card.Body>
-					<Card.Title style={{ color: '#437e85', fontSize: '35px' }}>
+					<Card.Title style={{ color: '#437e85', fontSize: '30px' }}>
 						{title}
 					</Card.Title>
-					<Card.Text style={{ color: '#525252' }}>{description}</Card.Text>
-					<Button style={{ backgroundColor: '#ffcecf', border: 'none' }}>
-						More info
-					</Button>
+					<Card.Text style={{ color: '#525252', fontSize: '14px' }}>
+						{description}
+					</Card.Text>
+					<Row
+						className='align-items-center'
+						style={{ padding: '0px', margin: '0px' }}
+					>
+						<Col lg={5} style={{ margin: '0px', padding: '0px' }}>
+							<Button
+								style={{
+									backgroundColor: '#ffcecf',
+									border: 'none',
+									fontSize: '14px',
+								}}
+							>
+								More info
+							</Button>
+						</Col>
+						<Col lg={7} style={{ margin: '0px', padding: '0px' }}>
+							<Button
+								style={{
+									backgroundColor: '#1cab7c',
+									border: 'none',
+									fontSize: '14px',
+								}}
+							>
+								Add to Garden
+							</Button>
+						</Col>
+					</Row>
 				</Card.Body>
-				<small className='text-muted'>Last updated 3 mins ago</small>
+				<small className='text-muted' style={{ fontSize: '12px' }}>
+					Last updated 3 mins ago
+				</small>
 				{/* <Card.Footer>
 					<small className='text-muted'>Last updated 3 mins ago</small>
 				</Card.Footer> */}
