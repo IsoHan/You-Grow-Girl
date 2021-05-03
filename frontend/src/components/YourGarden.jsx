e36a37973a21da0a7fb140b691a1ab56297d49b3
Issy Hanna <ih315@cam.ac.uk>
2021-05-03 17:21:29 +0100

    add garden plant cards for user to Your garden page

--- frontend/src/components/YourGarden.jsx
+++ frontend/src/components/YourGarden.jsx
@@ -4,7 +4,8 @@ import CardSingle from './CardSingle';
 
 const YourGarden = () => {
     const [data, setData] = useState([]);
-    const token = '29f6e74ff332eb85c228498ad38e7df7122260aa'
+    const [mounted,setMounted] = useState(false)
+    const token = '0cbd3ad53963332da92f39ef72e321fc821ce87a'
     const getData = async () => {
 		const res = await axios.get(`http://127.0.0.1:8000/gardens/api/gardenplants/`,{
             headers: {
@@ -22,11 +23,14 @@ const YourGarden = () => {
 			</div>
 		));
 		setData(postData);
-
+            ;
     };
     useEffect(() => {
-		getData();
-	},);
+		if (mounted === false){
+            getData();
+            setMounted(true)
+        } 
+	},[mounted]);
     return (
         <div>
             <div className="cards">{data}</div>
