e1596af6688e5ed6e541050c4d3f8f10d698eb15
Issy Hanna <ih315@cam.ac.uk>
2021-05-03 15:26:07 +0100

    add return json data for users garden plants

--- backend/YouGrowGirl/gardens/serializers.py
+++ backend/YouGrowGirl/gardens/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from gardens.models import GardenPlant
+from plants.serializers import PlantSerializer
 
 # Plant Serializer
 class GardenPlantSerializer(serializers.ModelSerializer):
+    plant = PlantSerializer()
     class Meta:
+        fields = ('plant','owner' )
         model = GardenPlant
-        fields = '__all__'
+ 
+class GardenPlantAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields= ('plant',)
+        model= GardenPlant 
         
