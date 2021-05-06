361bad0f082726671b3730f6039384fe6b28b059
aselperera <aselperera@hotmail.com>
2021-05-06 22:30:32 +0100

    Added styling for empty garden page

--- frontend/src/components/YourGarden.jsx
+++ frontend/src/components/YourGarden.jsx
@@ -5,13 +5,16 @@ import '../App.css';
 import GardenPlantCard from './GardenPlantCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Pagination from 'react-bootstrap/Pagination';
+import plants from '../images/threeplants.png';
 
 function YourGarden() {
 	const [offset, setOffset] = useState(0);
 	const [data, setData] = useState([]);
 	const [perPage] = useState(3);
 	const [pageCount, setPageCount] = useState(0);
+	const [showImage, setShowImage] = useState(true);
 
 	const getData = async () => {
 		const res = await axios.get(
@@ -25,25 +28,31 @@ function YourGarden() {
 		const data = res.data;
 		const slice = data.slice(offset * perPage, offset * perPage + perPage);
 
-		if (slice.length===0){
-			const noPlants = <p>You don't currently have any plants growing in you garden. Please look through our 
-				plant collection to add plants to your garden. 
-			</p>
-			setData(noPlants)
-		}else{
-		const postData = slice.map((pd,index) => (
-			<GardenPlantCard
-				key={index}
-				id={pd.id}
-				plant_id={pd.plant.id}
-				title={pd.plant.common_name}
-				description={pd.plant.description}
-				image={pd.plant.image}
-				handleDeletion={handleDeletion}
-			/>
-		));
-		setData(postData);
-		setPageCount(Math.ceil(data.length / perPage));}
+		if (slice.length === 0) {
+			const noPlants = (
+				<Col>
+					You don't currently have any plants growing in your garden! Please
+					look through our plant collection to add plants to your garden.
+				</Col>
+			);
+			setData(noPlants);
+			setShowImage(true);
+		} else {
+			const postData = slice.map((pd, index) => (
+				<GardenPlantCard
+					key={index}
+					id={pd.id}
+					plant_id={pd.plant.id}
+					title={pd.plant.common_name}
+					description={pd.plant.description}
+					image={pd.plant.image}
+					handleDeletion={handleDeletion}
+				/>
+			));
+			setData(postData);
+			setShowImage(false);
+			setPageCount(Math.ceil(data.length / perPage));
+		}
 	};
 
 	const handlePageClick = (e) => {
@@ -70,25 +79,33 @@ function YourGarden() {
 	}
 
 	return (
-		<Container>
-			<Row>{data}</Row>
-			<div className='pagination-container'>
-				<ReactPaginate
-					previousLabel={'<'}
-					nextLabel={'>'}
-					breakLabel={'...'}
-					breakClassName={'break-me'}
-					pageCount={pageCount}
-					marginPagesDisplayed={3}
-					pageRangeDisplayed={2}
-					onPageChange={handlePageClick}
-					containerClassName={'pagination'}
-					subContainerClassName={'pages pagination'}
-					activeClassName={'active'}
-				/>
-			</div>
-
-		</Container>
+		<div>
+			<Container style={{ textAlign: 'center' }}>
+				<h2
+					className='page-title text-center'
+					style={{ marginTop: '20px', marginBottom: '15px' }}
+				>
+					Your Garden
+				</h2>
+				<Row>{data}</Row>
+				<div className='pagination-container'>
+					<ReactPaginate
+						previousLabel={'<'}
+						nextLabel={'>'}
+						breakLabel={'...'}
+						breakClassName={'break-me'}
+						pageCount={pageCount}
+						marginPagesDisplayed={3}
+						pageRangeDisplayed={2}
+						onPageChange={handlePageClick}
+						containerClassName={'pagination'}
+						subContainerClassName={'pages pagination'}
+						activeClassName={'active'}
+					/>
+				</div>
+			</Container>
+			{showImage && <img src={plants} alt={''} className='threeplants' />}
+		</div>
 	);
 }
 
