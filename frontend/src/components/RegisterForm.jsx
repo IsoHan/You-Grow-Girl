6557d6544a5bdef6c2462ffde0de52b87bf357e0
aselperera <aselperera@hotmail.com>
2021-05-07 09:26:59 +0100

    Register successful message

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
-import plants from "../images/threeplants.png";
-import axiosInstance from "./axiosFetch";
-import validate from "./validateRegister";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import plants from '../images/threeplants.png';
+import axiosInstance from './axiosFetch';
+import validate from './validateRegister';
+import { store } from 'react-notifications-component';
 
 const Register = (props) => {
 	const history = useHistory();
 	const [details, setDetails] = useState({
-		username: "",
-		email: "",
-		password: "",
-		password1: "",
+		username: '',
+		email: '',
+		password: '',
+		password1: '',
 	});
 	const [errors, setErrors] = useState({});
 	const [isSubmitting, setIsSubmitting] = useState(false);
@@ -24,7 +25,7 @@ const Register = (props) => {
 	useEffect(() => {
 		if (Object.keys(errors).length === 0 && isSubmitting) {
 			axiosInstance
-				.post("/register", {
+				.post('/register', {
 					username: details.username,
 					email: details.email,
 					password: details.password,
@@ -32,27 +33,41 @@ const Register = (props) => {
 					// Refer to validateRegister.jsx
 				})
 				.then((res) => {
-					history.push("/signin/");
+					history.push('/signin/');
 					console.log(res);
 					console.log(res.data);
 				});
+
+			store.addNotification({
+				title: 'Registration Successful',
+				message: 'You have been successfully registered! You may now log in.',
+				type: 'success',
+				insert: 'top',
+				container: 'top-right',
+				animationIn: ['animate__animated', 'animate__fadeIn'],
+				animationOut: ['animate__animated', 'animate__fadeOut'],
+				dismiss: {
+					duration: 5000,
+					onScreen: true,
+				},
+			});
 		}
 	}, [errors]);
 
 	return (
-		<div className="font-link">
+		<div className='font-link'>
 			<form onSubmit={handleSubmit}>
-				<div className="form-inner">
-					<h2 className="page-title">
+				<div className='form-inner'>
+					<h2 className='page-title'>
 						<b>Register</b>
 					</h2>
-					<div className="form-group">
-						<label htmlFor="username">Create Username:</label>
+					<div className='form-group'>
+						<label htmlFor='username'>Create Username:</label>
 						<input
-							type="text"
-							username="username"
-							id="username"
-							placeholder="Enter your username"
+							type='text'
+							username='username'
+							id='username'
+							placeholder='Enter your username'
 							onChange={(e) =>
 								setDetails({ ...details, username: e.target.value })
 							}
@@ -61,13 +76,13 @@ const Register = (props) => {
 						{errors.username && <p>{errors.username}</p>}
 					</div>
 
-					<div className="form-group">
-						<label htmlFor="email">Email:</label>
+					<div className='form-group'>
+						<label htmlFor='email'>Email:</label>
 						<input
-							type="text"
-							email="email"
-							id="email"
-							placeholder="Enter your email"
+							type='text'
+							email='email'
+							id='email'
+							placeholder='Enter your email'
 							onChange={(e) =>
 								setDetails({ ...details, email: e.target.value })
 							}
@@ -76,13 +91,13 @@ const Register = (props) => {
 						{errors.email && <p>{errors.email}</p>}
 					</div>
 
-					<div className="form-group">
-						<label htmlFor="password">Create Password: </label>
+					<div className='form-group'>
+						<label htmlFor='password'>Create Password: </label>
 						<input
-							type="password"
-							password="password"
-							id="password"
-							placeholder="Enter your password"
+							type='password'
+							password='password'
+							id='password'
+							placeholder='Enter your password'
 							onChange={(e) =>
 								setDetails({ ...details, password: e.target.value })
 							}
@@ -90,13 +105,13 @@ const Register = (props) => {
 						/>
 						{errors.password && <p>{errors.password}</p>}
 					</div>
-					<div className="form-group">
-						<label htmlFor="password">Confirm Password: </label>
+					<div className='form-group'>
+						<label htmlFor='password'>Confirm Password: </label>
 						<input
-							type="password"
-							password="password1"
-							id="password1"
-							placeholder="Confirm password here"
+							type='password'
+							password='password1'
+							id='password1'
+							placeholder='Confirm password here'
 							onChange={(e) =>
 								setDetails({ ...details, password1: e.target.value })
 							}
@@ -105,18 +120,18 @@ const Register = (props) => {
 						{errors.password1 && <p>{errors.password1}</p>}
 					</div>
 				</div>
-				<input type="submit" value="Sign Up Now!" />
+				<input type='submit' value='Sign Up Now!' />
 				<div>
 					<Link
-						to="/signin"
-						className="link"
-						style={{ textDecoration: "none" }}
+						to='/signin'
+						className='link'
+						style={{ textDecoration: 'none' }}
 					>
 						Click here to log in
 					</Link>
 				</div>
 			</form>
-			<img src={plants} alt={"!"} className="threeplants" />
+			<img src={plants} alt={'!'} className='threeplants' />
 		</div>
 	);
 };
