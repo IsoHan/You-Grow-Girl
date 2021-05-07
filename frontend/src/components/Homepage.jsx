2c7656eee4fe98b58c0de5ba098f7eabd97880f4
aselperera <aselperera@hotmail.com>
2021-05-07 08:59:01 +0100

    Updated styling

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,30 +1,31 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
-import Carousel from "react-bootstrap/Carousel";
-import { NavLink } from "react-router-dom";
-import homepageimage from "../images/homepageimage.png";
-import search from "../images/search.png";
-import planticon2 from "../images/planticon2.png";
-import plant1 from "../images/plant1.png";
-import plant2 from "../images/plant2.png";
-import plant3 from "../images/plant3.png";
-import HomeCard from "./HomeCard";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
+import { NavLink } from 'react-router-dom';
+import homepageimage from '../images/homepageimage.png';
+import search from '../images/search.png';
+import planticon2 from '../images/planticon2.png';
+import plant1 from '../images/plant1.png';
+import plant2 from '../images/plant2.png';
+import plant3 from '../images/plant3.png';
+import HomeCard from './HomeCard';
 import CarouselComp from './CarouselComp';
 
 const Homepage = () => {
 	const [data, setData] = useState([]);
-	
+
 	const getData = async () => {
 		const res = await axios.get(`http://127.0.0.1:8000/api/plants/`);
 		const data2 = res.data;
-		console.log(data2)
-		const weeklyPlants = data2.filter((pd) => (
-			pd.id === 11 || pd.id === 31 || pd.id === 25 ))
+		console.log(data2);
+		const weeklyPlants = data2.filter(
+			(pd) => pd.id === 11 || pd.id === 31 || pd.id === 25
+		);
 		const postData = weeklyPlants.map((pd) => (
 			<CarouselComp
 				title={pd.common_name}
@@ -34,37 +35,49 @@ const Homepage = () => {
 			/>
 		));
 		setData(postData);
-	}
-	
+	};
+
 	useEffect(() => {
-		getData();	
+		getData();
 	}, []);
-	
+
 	return (
-		<div className="font-link">
+		<div className='font-link'>
 			<Container>
 				<Row>
 					<Col
 						md={{ span: 6, order: 1 }}
 						xs={{ span: 12, order: 12 }}
-						className="homepage-top"
+						className='homepage-top'
 					>
 						<div>
-							<h2 className="page-title">
+							<h2 className='page-title' style={{ color: '#437e85' }}>
 								<b>Plants made easy</b>
 							</h2>
 							<br />
-							<p style={{ fontSize: "20px" }}>
-								You Grow Girl helps you get the best for your planty friend
+							<p style={{ fontSize: '20px' }}>
+								You Grow Girl helps you get the best for your floral friends
 							</p>
-							<p>
-								Here at You Grow Girl we help you care for your plants in the best possible way. <br/>
-								Find information about the plants that will suit you best, and how to grow them to be healthy, tall, and strong. <br/>
-								Discover Your Garden to save your plants and keep all of your information in one convenient place.
+							<hr />
+							<p style={{ fontSize: '20px' }}>
+								<ul>
+									<li>
+										Here at You Grow Girl we help you care for your plants in
+										the best possible way
+									</li>
+									<li>
+										Find information about the plants that will suit you best,
+										and how to grow them to be healthy, tall, and strong
+									</li>
+									<li>
+										Discover Your Garden to save your plants and keep all of
+										your information in one convenient place
+									</li>
+								</ul>
 							</p>
-							<NavLink to="/signin">
+							<NavLink to='/signin'>
 								<Button
-									style={{ backgroundColor: "#1cab7c", borderColor: "#1cab7c" }}
+									style={{ backgroundColor: '#1cab7c', borderColor: '#1cab7c' }}
 								>
 									Sign In
 								</Button>
@@ -72,66 +85,73 @@ const Homepage = () => {
 						</div>
 					</Col>
 					<Col md={{ span: 6, order: 12 }} xs={{ span: 12, order: 1 }}>
-						<Image src={homepageimage} alt="Assorted plants" fluid />
+						<Image src={homepageimage} alt='Assorted plants' fluid />
 					</Col>
 				</Row>
 				<br />
 				<Row>
-					<Col md={6} xs={12} className="home-icons">
+					<Col md={6} xs={12} className='home-icons'>
 						<Image
 							src={search}
-							alt="Search icon"
-							style={{ height: "150px" }}
+							alt='Search icon'
+							style={{ height: '150px' }}
 							fluid
 						/>
 						<p>Search our database for your plant</p>
-						<h6 className="font-two">
+						<h6 className='font-two'>
 							Get instructions on how to care for your plant
 						</h6>
 					</Col>
-					<Col md={6} xs={12} className="home-icons">
+					<Col md={6} xs={12} className='home-icons'>
 						<Image
 							src={planticon2}
-							alt="Search icon"
-							style={{ height: "150px" }}
+							alt='Search icon'
+							style={{ height: '150px' }}
 							fluid
 						/>
 						<p>Add plants to your garden</p>
-						<h6 className="font-two">
+						<h6 className='font-two'>
 							For regular access and watering reminders
 						</h6>
 					</Col>
 				</Row>
 				<br />
 				<Row>
-					<Carousel indicators={false} nextIcon={false} prevIcon={false} pause={'hover'} interval={3000} fade={true}>
-						<Carousel.Item>
-							{data[0]}
-						</Carousel.Item>
-						<Carousel.Item>
-							{data[1]}
-						</Carousel.Item>
-						<Carousel.Item>
-							{data[2]}
-						</Carousel.Item>
+					<Carousel
+						indicators={false}
+						nextIcon={false}
+						prevIcon={false}
+						pause={'hover'}
+						interval={3000}
+						fade={true}
+					>
+						<Carousel.Item>{data[0]}</Carousel.Item>
+						<Carousel.Item>{data[1]}</Carousel.Item>
+						<Carousel.Item>{data[2]}</Carousel.Item>
 					</Carousel>
 				</Row>
 				<br />
-				<Row>
-					<Col lg={4} md={6} sm={12} >
-						<HomeCard 
-						text="Learn to grow your plants from a seed in the best conditions for them" 
-						image={plant1} fluid />
+				<Row style={{ marginTop: '60px', marginBottom: '20px' }}>
+					<Col lg={4} md={6} sm={12}>
+						<HomeCard
+							text='Learn to grow your plants from a seed in the best conditions for them'
+							image={plant1}
+							fluid
+						/>
 					</Col>
-					<Col lg={4} md={6} sm={12} >
-						<HomeCard 
-						text="Care for your plants with proper sunlight conditions, water schedule, and know when they'll bloom" 
-						image={plant2} fluid />
+					<Col lg={4} md={6} sm={12}>
+						<HomeCard
+							text="Care for your plants with proper sunlight conditions, water schedule, and know when they'll bloom"
+							image={plant2}
+							fluid
+						/>
 					</Col>
-					<Col lg={4} md={6} sm={12} >
-						<HomeCard 
-						text="Reap the rewards of your gorgeous, healthy plants!" 
-						image={plant3} fluid />
+					<Col lg={4} md={6} sm={12}>
+						<HomeCard
+							text='Reap the rewards of your gorgeous, healthy plants!'
+							image={plant3}
+							fluid
+						/>
 					</Col>
 				</Row>
 			</Container>
